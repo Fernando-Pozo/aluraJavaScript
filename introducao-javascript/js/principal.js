fcc0d94cb9b6790749d6f14dccfd5317da1f2058
Fernando-Pozo <fernandopozomqs@gmail.com>
2021-12-11 15:11:37 -0300

    melhorando o calculo do imc

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,25 +1,35 @@
-var paciente = document.querySelector("#primeiro-paciente");
-var tdPeso = paciente.querySelector(".info-peso");
-var tdAltura = paciente.querySelector(".info-altura");
+var paciente = document.querySelectorAll(".paciente");
 
-var peso = tdPeso.textContent;
-var altura = tdAltura.textContent;
+for (var i = 0; i < paciente.length; i++) {
+     
+    var tdPeso = paciente[i].querySelector(".info-peso");
+    var tdAltura = paciente[i].querySelector(".info-altura");
 
-var tdImc = paciente.querySelector(".info-imc");
+    var peso = tdPeso.textContent;
+    var altura = tdAltura.textContent;
 
-var pesoEhValido = true;
-var alturaEhValida = true; 
+    var tdImc = paciente[i].querySelector(".info-imc");
 
-if(peso <= 0 || peso >= 1000){
-    console.log("Peso inválido");
-    pesoEhValido = false;
-}else if (altura <= 0 || altura >= 3.00){
-    console.log("Altura inválida");
-    alturaEhValida = false;
-} 
-if(pesoEhValido && alturaEhValida){
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc;
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+
+    if (peso <= 0 || peso >= 1000) {
+        console.log("Peso inválido");
+        pesoEhValido = false;
+        tdImc.textContent = "peso inválido";
+        paciente[i].classList.add("paciente-invalido");
+        
+    } else if (altura <= 0 || altura >= 3.00) {
+        console.log("Altura inválida");
+        alturaEhValida = false;
+        tdImc.textContent = "Altura inválido";
+        paciente[i].classList.add("paciente-invalido");
+    }
+    if (pesoEhValido && alturaEhValida) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
 }
 
-console.log(imc);
+
+
